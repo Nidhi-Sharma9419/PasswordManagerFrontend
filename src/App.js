004814090b4a56eb27046478be30db0c98f0080d
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-18 11:38:12 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+
+
+import { AddPass } from './Components/AddPass';
+import React, {useState} from 'react';
 
 function App() {
+  
+
+  const addPass = (website, title, desc)=>{
+    console.log("Adding this task...",website,title, desc)
+    let sno;
+    
+    const myList = {
+      sno: sno,
+      website: website,
+      title: title,
+      desc: desc,
+    }
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title="Password Manager" searchBar="true" />
+    <AddPass addPass={addPass}/>
+   
+    
+    </>
   );
 }
 
---
+++ src/Components/AddPass.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+
+export const AddPass = ({addPass}) => {
+    const [website, setWebsite] = useState("");
+    const [title, setTitle] = useState("");
+    const [desc, setdesc] = useState("");
+    const submit = (e)=>{
+        e.preventDefault();
+        if(!website || !title || !desc){
+            alert("Website or Title or Description cannot be blank")
+        }
+        addPass(website,title, desc);
+    }
+    return (
+        <div className='container my-3'>
+            <h3>
+                Manager 
+            </h3>
+            <form onSubmit={submit}>
+                <div className='mb-3'>
+                    <label htmlfor="website" className='form-label'>Website Name</label>
+                    <input type="text" action="https://example.com" method="post" value = {website} onChange={(e)=>setWebsite(e.target.value)} className='form-control' id="website"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlfor="title" className="form-label">Username</label>
+                    <input type="text" value = {title} onChange={(e)=>setTitle(e.target.value)} className="form-control" id="title" aria-describedby="emailHelp"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlfor="desc" className="form-label">Password</label>
+                    <input type="password" value = {desc} onChange={(e)=>setdesc(e.target.value)} className="form-control" id="desc"/>
+                </div>
+                
+                <button type="submit" className="btn btn-primary btn-sm" >Add</button>
+            </form>
+        </div>
+    )
+}
